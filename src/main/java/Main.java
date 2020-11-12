e407c85f12b23c78c5ea63fc52fcfb287a392c0c
Sébastien Mosser <mosser.sebastien@uqam.ca>
2020-11-12 10:57:03 -0500

    Success / Failure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,20 @@
+import tests.*;
+import uutf.TestRunner;
+
 import static uutf.Assertions.*;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Démo JUNIT");
-        try {
-            System.out.println("Test case: AddIntegers");
-            int x = 1;
-            int y=1;
-            assertEquals(2, x + y);
-            System.out.println("SUCCESS!");
-        } catch(AssertionError ae) {
-            System.out.println("FAILED!");
-        }
 
-        try {
-            System.out.println("Test case: SubIntegers");
-            int x = 1;
-            int y=1;
-            assertEquals(2, x - y);
-            System.out.println("SUCCESS!");
-        } catch(AssertionError ae) {
-            System.out.println("FAILED!");
-        }
+        TestRunner runner = new TestRunner();
 
+        runner.add(new AddIntegers());
+        runner.add(new SubIntegers());
+        runner.add(new ThrowAnException());
+
+        runner.run();
     }
 
 }
---
+++ src/main/java/uutf/TestCase.java
@@ -0,0 +1,22 @@
+package uutf;
+
+import static uutf.Assertions.assertEquals;
+
+public abstract  class  TestCase {
+
+    public final TestResult run() {
+        TestResult result = new TestResult(this.getClass().getCanonicalName());
+        try {
+            test();
+            result.setStatus(STATUS.PASSED);
+        } catch(AssertionError ae) {
+            result.setStatus(STATUS.FAILED);
+        } catch (Exception e) {
+            result.setStatus(STATUS.ERRORED);
+        }
+        return result;
+    }
+
+    protected abstract void test(); // Template Method
+
+}
